12adc7f0fd535bb0bf232a29f821e0d918d9517a
M. Abdul Ghofur <algar0940@gmail.com>
2024-04-03 03:22:06 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,19 @@
+const nama= "ghofur"
+let usia = 20;
+console.log(`nama saya adalah ${nama} Nim:${usia}`);
+
+function Biodata() {
+    if(usia > 10 && usia < 18 ) {
+        console.log('saya remaja')
+    }
+    else{ console.log('aku siapa')
+
+    }
+}
+
+Biodata();
+
+
+
+
+
